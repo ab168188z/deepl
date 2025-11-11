4df0260997e98d8c35dbf0bc410ae8356973b8e8
ab168188z <ttle30674@gmail.com>
2025-11-11 23:06:14 +0800

    Add files via upload

---
+++ static/js/head.js
@@ -0,0 +1,9 @@
+﻿
+function writelnLOGO(){
+        document.writeln('<img src="../images/logo2.png"/*tpa=http://download.fenhao.me/images/logo2.png*/ alt="">');
+
+}
+
+function writeLOGObody(){
+        document.writeln('<br /><img src="../images/logo2.png"/*tpa=http://download.fenhao.me/images/logo2.png*/ />');
+}
---
+++ static/js/quanzhan.js
@@ -0,0 +1,8 @@
+﻿
+host = window.location.host;
+
+
+if (host == "rj.taizlkj.cn") 
+{
+	document.writeln("长沙泰篆了科技有限公司 电话：4006886515  地址：湖南省长沙市岳麓区岳麓街道南二环一段518号阳光壹佰新城1号地块A1区1-2栋301房439号 备案号：湘ICP备2024044028号-1");
+}
